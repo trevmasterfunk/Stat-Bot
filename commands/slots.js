4f1a2769b8ef35cafa692c091add3bbc62aaab15
trevmasterfunk <trevorvixx0@gmail.com>
2021-11-06 13:17:10 -0500

    Added multispin functionality to slots

--- commands/slots.js
+++ commands/slots.js
@@ -4,7 +4,7 @@ const lib = require('../lib.js')
 
 module.exports = {
     name: 'slots',
-    description: "This command plays the slots. You can enter the ammount you'd like to bet at the end of the command ie. '-slots 60'. If you get two of the same emote in a row you win 5X bet. If you get three of the same emote you get 10X bet. Jackpot is 100X bet. Syntax: '-slots 10'",
+    description: "This command plays the slots. You can enter the ammount you'd like to bet at the end of the command ie. '-slots 60'. You can also add the number of times you'd like to spin to the end of your command ie. '-slots 50 spins 3'. If you get three of the same emote along a diagonal you win 3X bet. If you get two of the same emote in a row you win 5X bet. If you get three of the same emote you get 10X bet. Jackpot is 100X bet.",
     async execute(message, args, client) {
         //get store data
         let storepath = "./data/store.json"
@@ -23,8 +23,10 @@ module.exports = {
         }
 
         //create message
+        let divider = "---------------------------------------------------------------------------------------------"
         const embeddedmsg = new MessageEmbed()
             .setTitle('Slot Results - ' + message.author.username)
+            .setDescription(divider)
 
         //initialize reply
         let reply = "Something went wrong."
@@ -45,6 +47,20 @@ module.exports = {
                     message.channel.send({ embed: embeddedmsg })
                     return
                 }
+            }
+        }
+
+
+        let spins = 1
+        if (args[1] == "spins") {
+            let spinsrequest = parseInt(args[2], 10)
+            if (0 < spinsrequest && spinsrequest <= 5) {
+                spins = spinsrequest
+            } else {
+                reply = "Spins must be between 1 and 5!"
+                embeddedmsg.setDescription(reply)
+                message.channel.send({ embed: embeddedmsg })
+                return
             }
         }
 
@@ -86,86 +102,99 @@ module.exports = {
         const choices = store.games["Slots"].emotes
         const numchoices = Object.keys(choices).length - 1
 
-        //generate random rolls
-        let rand = []
-        rand.push(getrandom(numchoices))
-        rand.push(getrandom(numchoices))
-        rand.push(getrandom(numchoices))
 
-        //generate emotes message
-        let slotmachineresults = ""
-        for (i = 1; i < 4; i++) {
-            let line = ""
-            if (i == 1) {
-                //get first row
-                let s1i = rand[0] - 1
-                if (s1i < 0) { s1i = numchoices }
-                let s2i = rand[1] - 1
-                if (s2i < 0) { s2i = numchoices }
-                let s3i = rand[2] - 1
-                if (s3i < 0) { s3i = numchoices }
-                let e1 = await message.guild.emojis.cache.find(emoji => emoji.name == choices[s1i])
-                let e2 = await message.guild.emojis.cache.find(emoji => emoji.name == choices[s2i])
-                let e3 = await message.guild.emojis.cache.find(emoji => emoji.name == choices[s3i])
-                line = `${e1}` + `${e2}` + `${e3}` + "\n"
-            } else if (i == 2) {
-                //middle row
-                let s1i = rand[0]
-                let s2i = rand[1]
-                let s3i = rand[2]
-                let e1 = await message.guild.emojis.cache.find(emoji => emoji.name == choices[s1i])
-                let e2 = await message.guild.emojis.cache.find(emoji => emoji.name == choices[s2i])
-                let e3 = await message.guild.emojis.cache.find(emoji => emoji.name == choices[s3i])
-                line = `${e1}` + `${e2}` + `${e3}` + "\n"
-            } else {
-                //last row
-                let s1i = rand[0] + 1
-                if (s1i > numchoices) { s1i = 0 }
-                let s2i = rand[1] + 1
-                if (s2i > numchoices) { s2i = 0 }
-                let s3i = rand[2] + 1
-                if (s3i > numchoices) { s3i = 0 }
-                let e1 = await message.guild.emojis.cache.find(emoji => emoji.name == choices[s1i])
-                let e2 = await message.guild.emojis.cache.find(emoji => emoji.name == choices[s2i])
-                let e3 = await message.guild.emojis.cache.find(emoji => emoji.name == choices[s3i])
-                line = `${e1}` + `${e2}` + `${e3}`
+        let totalwager = 0
+        let totalpayout = 0
+        for (let j = 1; j <= spins; j++) {
+            //generate random rolls
+            let rand = []
+            rand.push(getrandom(numchoices))
+            rand.push(getrandom(numchoices))
+            rand.push(getrandom(numchoices))
+
+            //generate emotes message
+            let slotmachineresults = ""
+            for (i = 1; i < 4; i++) {
+                let line = ""
+                if (i == 1) {
+                    //get first row
+                    let s1i = rand[0] - 1
+                    if (s1i < 0) { s1i = numchoices }
+                    let s2i = rand[1] - 1
+                    if (s2i < 0) { s2i = numchoices }
+                    let s3i = rand[2] - 1
+                    if (s3i < 0) { s3i = numchoices }
+                    let e1 = await message.guild.emojis.cache.find(emoji => emoji.name == choices[s1i])
+                    let e2 = await message.guild.emojis.cache.find(emoji => emoji.name == choices[s2i])
+                    let e3 = await message.guild.emojis.cache.find(emoji => emoji.name == choices[s3i])
+                    line = `${e1}` + `${e2}` + `${e3}` + "\n"
+                } else if (i == 2) {
+                    //middle row
+                    let s1i = rand[0]
+                    let s2i = rand[1]
+                    let s3i = rand[2]
+                    let e1 = await message.guild.emojis.cache.find(emoji => emoji.name == choices[s1i])
+                    let e2 = await message.guild.emojis.cache.find(emoji => emoji.name == choices[s2i])
+                    let e3 = await message.guild.emojis.cache.find(emoji => emoji.name == choices[s3i])
+                    line = `${e1}` + `${e2}` + `${e3}` + "\n"
+                } else {
+                    //last row
+                    let s1i = rand[0] + 1
+                    if (s1i > numchoices) { s1i = 0 }
+                    let s2i = rand[1] + 1
+                    if (s2i > numchoices) { s2i = 0 }
+                    let s3i = rand[2] + 1
+                    if (s3i > numchoices) { s3i = 0 }
+                    let e1 = await message.guild.emojis.cache.find(emoji => emoji.name == choices[s1i])
+                    let e2 = await message.guild.emojis.cache.find(emoji => emoji.name == choices[s2i])
+                    let e3 = await message.guild.emojis.cache.find(emoji => emoji.name == choices[s3i])
+                    line = `${e1}` + `${e2}` + `${e3}`
+                }
+                slotmachineresults = slotmachineresults + line
             }
-            slotmachineresults = slotmachineresults + line
+
+            //generate rest of response
+            let jackpot = store.games["Slots"].jackpot
+            let Payout = 0
+            let payoutresponse = "   No matches. Try again! Maybe if you bet more you'll win big :)"
+            let middle = rand[1]
+            if (choices[rand[0]] == jackpot && choices[rand[1]] == jackpot && choices[rand[2]] == jackpot) {
+                Payout = wager * 100
+                payoutresponse = "   JACKPOT!!!!!!!!! DING DING DING"
+            } else if (rand[0] == rand[1] && rand[0] == rand[2]) {
+                Payout = wager * 10
+                payoutresponse = "   Hot damn! Three in a row!"
+            } else if (rand[0] == rand[1] || rand[1] == rand[2]) {
+                Payout = wager * 5
+                payoutresponse = "  Ooo so close! Two in a row!"
+            } else if ((rand[0] == (middle - 1) && rand[2] == (middle + 1)) || rand[2] == (middle - 1) && rand[0] == (middle + 1)) {
+                Payout = wager * 3
+                payoutresponse = "  You hit a diagonal"
+            }
+
+            totalpayout += Payout
+            totalwager += wager
+
+            data.users[customerid].deductions = data.users[customerid].deductions + ((Payout - wager) * 60000)
+
+
+
+            //combine message into embed
+            embeddedmsg.addFields(
+                { name: "Spin " + j + " Results", value: slotmachineresults, inline: false },
+                { name: 'Wager', value: (wager), inline: true },
+                { name: 'Payout', value: (Payout), inline: true },
+                { name: "Response", value: payoutresponse + "\n" + divider, inline: false },
+            )
         }
 
-        //generate rest of response
-        let jackpot = store.games["Slots"].jackpot
-        let Payout = 0
-        let payoutresponse = "   No matches. Try again! Maybe if you bet more you'll win big :)"
-        let middle = rand[1]
-        if (choices[rand[0]] == jackpot && choices[rand[1]] == jackpot && choices[rand[2]] == jackpot) {
-            Payout = wager * 100
-            payoutresponse = "   JACKPOT!!!!!!!!! DING DING DING"
-        } else if (rand[0] == rand[1] && rand[0] == rand[2]) {
-            Payout = wager * 10
-            payoutresponse = "   Hot damn! Three in a row!"
-        } else if (rand[0] == rand[1] || rand[1] == rand[2]) {
-            Payout = wager * 5
-            payoutresponse = "  Ooo so close! Two in a row!"
-        } else if ((rand[0] == (middle - 1) && rand[2] == (middle + 1)) || rand[2] == (middle - 1) && rand[0] == (middle + 1)) {
-            Payout = wager * 3
-            payoutresponse = "  You hit a diagonal"
-        }
 
-        Payout = Payout * 60000
-        wager = wager * 60000
-
-        data.users[customerid].deductions = data.users[customerid].deductions + Payout - wager
-
-        //combine message into embed
-        embeddedmsg.setDescription(slotmachineresults)
+        //add new balance
         embeddedmsg.addFields(
-            { name: 'Wager', value: (wager / 60000), inline: true },
-            { name: 'Payout', value: (Payout / 60000), inline: true },
-            { name: "Response", value: payoutresponse, inline: false },
-            { name: "New Balance", value: Math.floor((lib.gettotaltime(customerid, data) / 60000)), inline: false }
+            { name: "Total Wager", value: totalwager, inline: true },
+            { name: "Total Payout", value: totalpayout, inline: true },
+            { name: "New Balance", value: Math.floor((lib.gettotaltime(customerid, data) / 60000)), inline: true }
         )
-
         // //save data
         globaluserdata = data
 
